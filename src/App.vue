9aec56857acd062e04af44b6540eacf5b2a780d7
gaoly <shuiruohanyu@foxmail.com>
2020-01-08 20:56:27 +0800

    搭建基础框架爱

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,8 @@
 <template>
   <div id="app">
-<van-button type="primary">主要按钮</van-button>
     <!-- 放置一个容器 -->
     <router-view />
   </div>
 </template>
-
 <style lang="less">
-p {
-  font-size: 20px;
-}
 </style>
--- src/views/layout/index.vue
+++ src/views/layout/index.vue
@@ -1,13 +1,51 @@
 <template>
-  <div>布局 </div>
+  <div class="container">
+    <van-nav-bar
+      v-if="path!=='/user'"
+      fixed
+      title="黑马头条"
+      right-text="搜索"
+    />
+     <!-- 二级路由容器 -->
+     <div class='my-wrapper' :class="{ noTop: path === '/user' }"
+>
+       <router-view ></router-view>
+     </div>
+     <van-tabbar  route>
+        <van-tabbar-item to="/" icon="home-o">首页</van-tabbar-item>
+        <van-tabbar-item to="/question" icon="chat-o">问答</van-tabbar-item>
+       <van-tabbar-item to="/video" icon="video-o">视频</van-tabbar-item>
+       <van-tabbar-item to="/user" icon="user-o">我的</van-tabbar-item>
+     </van-tabbar>
+  </div>
+
 </template>
 
 <script>
 export default {
-
+  computed: {
+    path () {
+      return this.$route.path
+    }
+  }
 }
 </script>
 
-<style>
-
+<style lang='less' scoped>
+.container{
+  width: 100%;
+  height: 100%;
+  position: relative;
+  .my-wrapper{
+    width: 100%;
+    height: 100%;
+    overflow: hidden;
+    padding-top: 46px;
+    padding-bottom: 50px;
+    box-sizing: border-box;
+    &.noTop{
+      padding-top: 0;
+    }
+  }
+}
 </style>
