8db90bd779ef1c474d9155df72810fc0eef91eee
gaoly <shuiruohanyu@foxmail.com>
2020-01-08 18:34:28 +0800

    新增路由组件

--- src/router/index.js
+++ src/router/index.js
@@ -1,19 +1,35 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '@/views/home'
-
+const Layout = () => import('@/views/layout')
+const Question = () => import('@/views/question')
+const Video = () => import('@/views/video')
+const User = () => import('@/views/user')
+const UserProfile = () => import('@/views/user/profile')
+const UserChat = () => import('@/views/user/chat')
+const Login = () => import('@/views/login')
+const Search = () => import('@/views/search')
+const SearchResult = () => import('@/views/search/result')
+const Article = () => import('@/views/article')
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    redirect: '/home'
+    component: Layout,
+    children: [
+      { path: '/', name: 'home', component: Home },
+      { path: '/question', name: 'question', component: Question },
+      { path: '/video', name: 'video', component: Video },
+      { path: '/user', name: 'user', component: User }
+    ]
   },
-  {
-    path: '/home',
-    name: 'home',
-    component: Home
-  }
+  { path: '/user/profile', name: 'user-profile', component: UserProfile },
+  { path: '/user/chat', name: 'user-chat', component: UserChat },
+  { path: '/login', name: 'login', component: Login },
+  { path: '/search', name: 'search', component: Search },
+  { path: '/search/result', name: 'search-result', component: SearchResult },
+  { path: '/article', name: 'article', component: Article }
   // {
   //   path: '/about',
   //   name: 'about',
---
+++ src/views/article/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>文章详情</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
