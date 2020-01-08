f76e0a2b231331df3f9b520e36f87c5bf39217e9
gaoly <shuiruohanyu@foxmail.com>
2020-01-08 17:15:07 +0800

    rem适配

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,13 @@
 <template>
   <div id="app">
+    <p >123456</p>
     <!-- 放置一个容器 -->
     <router-view />
   </div>
 </template>
 
 <style lang="less">
+p {
+  font-size: 20px;
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import 'amfe-flexible' // 引入动态设置的值
 Vue.config.productionTip = false
 
 new Vue({
