d0871f5e75af8332f205ca362d706edb7ad248e6
gaoly <shuiruohanyu@foxmail.com>
2020-01-09 23:31:35 +0800

    拦截器功能开发

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,24 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import * as auth from '@/utils/auth'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
+  state: {
+    user: auth.getUser()
+  },
+  mutations: {
+    // 更新用户信息
+    updateUser (state, payLoad) {
+      state.user = payLoad.user
+      auth.setUser(payLoad.user) // 设置数据到缓存
+    },
+    // 删除掉用户信息
+    delUser (state) {
+      state.user = {}
+      auth.delUser() // 删除存储信息
+    }
+  },
   getters: {},
   actions: {}
 })
--- src/utils/request.js
+++ src/utils/request.js
@@ -1,7 +1,30 @@
 import axios from 'axios'
-
+import JSONBig from 'json-bigint' // 用来转化数据
+import store from '@/store' // 用户存储数据
 // 创建一个新的axios实例
 const instance = axios.create({
-  baseURL: 'http://ttapi.research.itcast.cn/app/v1_0/'
+  baseURL: 'http://ttapi.research.itcast.cn/app/v1_0/',
+  transformResponse: [data => {
+    try {
+      return JSONBig.parse(data)
+    } catch (error) {
+      return data
+    }
+  }]
+})
+// 请求拦截器
+instance.interceptors.request.use(config => {
+  if (store.state.user.token) { config.headers.Authorization = `Bearer ${store.state.user.token}` }
+  return config
+}, err => Promise.reject(err))
+// 响应拦截器
+instance.interceptors.response.use(response => {
+  try {
+    return response.data.data
+  } catch (error) {
+    return response.data
+  }
+}, error => {
+  return Promise.reject(error)
 })
 export default instance
