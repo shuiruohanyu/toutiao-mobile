e696a8d0f570c413d8762872638b8f83199f51b5
gaoly <shuiruohanyu@foxmail.com>
2020-01-10 17:26:42 +0800

    重构获取频道API

--- src/api/channels.js
+++ src/api/channels.js
@@ -1,11 +1,33 @@
 import request from '@/utils/request'
+import store from '@/store'
+const CHANNELS_KEY = 'hm-toutiao-91-channel'
 /**
  * 获取我的频道数据
  * **/
 export function getMyChannels () {
-  return request({
-    url: 'user/channels'
-  }) // 获取数据
+  return new Promise(async function (resolve, reject) {
+    if (store.state.user.token) {
+      // 在登录情况下
+      let data = await request({
+        url: 'user/channels'
+      }) // 获取数据
+      resolve(data)
+    } else {
+      // 没有登录下
+      const cacheChannel = localStorage.getItem(CHANNELS_KEY)
+      if (cacheChannel) {
+        // 有缓存的情况下
+        resolve({ channels: JSON.parse(cacheChannel) })
+      } else {
+        // 没缓存的情况下
+        let data = await request({
+          url: 'user/channels'
+        }) // 获取数据
+        localStorage.setItem(CHANNELS_KEY, JSON.stringify(data.channels))
+        resolve(data)
+      }
+    }
+  })
 }
 // 获取所有频道
 
