404bde4f598e56541be00d40e5b173759f9a9e2d
gaoly <shuiruohanyu@foxmail.com>
2020-01-10 09:20:33 +0800

    完成文章的布局

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -4,9 +4,33 @@
       <van-tab v-for="index in 8" :title="'标签 ' + index" :key="index">
         <div class="scroll-wrapper">
           <!-- 另外一个下拉刷新组件 -->
-          <van-pull-refresh v-model="downLoading" @refresh="onRefresh" :success-text="refreshSuccessText">
+          <van-pull-refresh
+            v-model="downLoading"
+            @refresh="onRefresh"
+            :success-text="refreshSuccessText"
+          >
             <van-list v-model="loading" :finished="finished" finished-text="没有更多了" @load="onLoad">
-              <van-cell v-for="(item,i) in articles" :key="i">{{ '标签' + index + '---' + item}}</van-cell>
+              <van-cell v-for="(item,i) in articles" :key="i">
+                <div class="article_item">
+                  <h3 class="van-ellipsis">PullRefresh下拉刷新PullRefresh下拉刷新下拉刷新下拉刷新</h3>
+                  <div class="img_box">
+                    <van-image class="w33" fit="cover" src="https://img.yzcdn.cn/vant/cat.jpeg" />
+                    <van-image class="w33" fit="cover" src="https://img.yzcdn.cn/vant/cat.jpeg" />
+                    <van-image class="w33" fit="cover" src="https://img.yzcdn.cn/vant/cat.jpeg" />
+                  </div>
+                  <div class="img_box">
+                    <van-image class="w100" fit="cover" src="https://img.yzcdn.cn/vant/cat.jpeg" />
+                  </div>
+                  <div class="info_box">
+                    <span>你像一阵风</span>
+                    <span>8评论</span>
+                    <span>10分钟前</span>
+                    <span class="close">
+                      <van-icon name="cross"></van-icon>
+                    </span>
+                  </div>
+                </div>
+              </van-cell>
             </van-list>
           </van-pull-refresh>
           <!-- 用来做阅读记忆的 -->
@@ -123,6 +147,46 @@ export default {
     z-index: 1000;
     &::before {
       font-size: 20px;
+    }
+  }
+}
+.article_item{
+  h3{
+    font-weight: normal;
+    line-height: 2;
+  }
+  .img_box{
+    display: flex;
+    justify-content: space-between;
+    .w33{
+      width: 33%;
+      height: 90px;
+    }
+    .w100{
+      width: 100%;
+      height: 180px;
+    }
+  }
+  .info_box{
+    color: #999;
+    line-height: 2;
+    position: relative;
+    font-size: 12px;
+    span{
+      padding-right: 10px;
+      &.close{
+        border: 1px solid #ddd;
+        border-radius: 2px;
+        line-height: 15px;
+        height: 12px;
+        width: 16px;
+        text-align: center;
+        padding-right: 0;
+        font-size: 8px;
+        position: absolute;
+        right: 0;
+        top: 7px;
+      }
     }
   }
 }
